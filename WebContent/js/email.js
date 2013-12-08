90b9def9208d4c5f659ad89a75a86cc7ad52e924
Jack Wearden <jack@jackwearden.co.uk>
2013-12-08 01:26:02 +0000

    support sending messages, refactor auth

--- WebContent/js/email.js
+++ WebContent/js/email.js
@@ -1,25 +1,64 @@
 angular.module("email", [])
-  .factory("auth", function() {
-    function Auth (username, password) {
-      return true;
+  .factory("mail", function($http) {
+    return {
+      checkAuth: function CheckAuth (creds, fail, success) {
+        var authstring = "Basic " + btoa(creds.user + ":" + creds.pass);
+        $http.defaults.headers.common.Authorization = authstring; 
+
+        $http.get("messages")
+          .success(success)
+          .error(fail);
+        },
+      send: function Send (creds, message, fail, success) {
+        var authstring = "Basic " + btoa(creds.user + ":" + creds.pass);
+        $http.defaults.headers.common.Authorization = authstring; 
+
+        $http.post("messages", message)
+          .success(success)
+          .error(fail);
+      }
     };
-    return Auth;
   })
-  .controller("email", function($scope, auth) {
+  .controller("email", function($scope, mail) {
     $scope.loggedIn   = false;
     $scope.loginError = false;
+    $scope.sentModal  = 'hide';
+    $scope.sending    = false;
+    $scope.loading    = false;
     
     $scope.username = "";
     $scope.password = "";
     
     $scope.login = function() {
-      if (auth($scope.username, $scope.password)) {
-        $scope.loggedIn = true;
-        $scope.loginError = false;
-      } else {
-        $scope.loggedIn = false;
-        $scope.loginError = true;
+      function setState(success) {
+        return function () {
+          $scope.loggedIn   = success;
+          $scope.loginError = !success;
+          $scope.loading    = false;
+        };
       }
+
+      var success = setState(true);
+      var fail    = setState(false);
+
+      $scope.loading = true;
+      mail.checkAuth({user: $scope.username, pass: $scope.password}, fail, success);
     };
+    
+    var noop = function () {$scope.sending = false;};
+    
+    $scope.send = function() {
+      $scope.sending = true;
+      mail.send({user: $scope.username, pass: $scope.password}, {
+        to: $scope.to.split(/,? +/),
+        subject: $scope.subject,
+        message: $scope.message
+      }, noop, function () {
+    	  $scope.to = "";
+    	  $scope.subject = "";
+    	  $scope.message = "";
+    	  $scope.sending = false;
+      });
+    };	
   })
   ;
